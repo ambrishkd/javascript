76914991ec97f03edace03046f4e2c114f94d9ff
ambrishkd <dubey.ambrishdubey@gmail.com>
2024-11-01 19:53:05 +0530

    Add basics of object-oriented

---
+++ 09_oop/01_oop_basics.js
@@ -0,0 +1,29 @@
+/*
+
+Q: Do Js really have classes?
+A: Yes, Js does have classes. This was introduced in ES6. However, it is important to note that Js is
+    primarily a prototype-based language, and its classes are primarily syntactic sugar over existing
+    prototype-based inheritance mechanisms. 
+    In other words, it provides a more familiar syntac for developers coming from class-based languages
+    such as C++ or Java, but under the hood it works somewhat differently.
+    
+- Object: 
+    Collection of properties and methods
+
+Q: Why use OOP?
+A: To avoid spaghetti code and to avoid messed up code
+
+- Parts of OOP:
+    Object literal
+    Constructor function
+    Prototypes
+    Classes
+    Instances(new, this)
+
+- 4 pillars:
+    Inheritance
+    Abstraction
+    Encapsulation
+    Polymorphism
+
+*/
